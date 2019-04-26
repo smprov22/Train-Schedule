74cd46f113f8a5d634ddcd558cf617cfc983b19b
smprov22 <smprovence22@gmail.com>
2019-04-26 11:23:32 -0700

    final changes

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -28,8 +28,6 @@ $("#add-train").on("click", function(event) {
     firstStart: firstStart,
     frequency: frequency,
   });
-
-  alert("Train Successfully Added");
   
   $("#trainName").val("");
   $("#destination").val("");
@@ -42,54 +40,47 @@ $("#add-train").on("click", function(event) {
 database.ref().on("child_added", function(snapshot) {
   // storing the snapshot.val() in a variable for convenience
   var sv = snapshot.val();
-
-  // Console.loging the last user's data
-  console.log(sv.name);
-  console.log(sv.destination);
-  console.log(sv.firstStart);
-  console.log(sv.frequency);
   
   var firstStartConverted = moment(sv.firstStart, "HH:mm").subtract(1, "years");
-  console.log(firstStartConverted);
-  
+    
     // Current Time
   var currentTime = moment();
   console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
   // Difference between the times
   var diffTime = moment().diff(moment(firstStartConverted), "minutes");
-  console.log("DIFFERENCE IN TIME: " + diffTime);
 
   // Time apart (remainder)
   var tRemainder = diffTime % sv.frequency;
-  console.log(tRemainder);
 
   // Minute Until Train
   var tMinutesTillTrain = sv.frequency - tRemainder;
-  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
   // Next Train
   var nextTrain = moment().add(tMinutesTillTrain, "minutes");
   var arrivalTime = moment(nextTrain).format("hh:mm A");
-  console.log("ARRIVAL TIME: " + arrivalTime);
   
   // Change the HTML to reflect
-  $("#info").append('<tr><td>' + sv.name + '</td><td>' + sv.destination + '</td><td>Every ' + sv.frequency + ' minutes</td><td>' + arrivalTime + '</td><td>' + tMinutesTillTrain + '</td></tr>');
-  
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(sv.name),
+    $("<td>").text(sv.destination),
+    $("<td>").text(sv.frequency),
+    $("<td>").text(arrivalTime),
+    $("<td>").text(tMinutesTillTrain),
+    $("<button>").text("X").addClass("remove-button").attr("id", snapshot.key).attr("title", "Delete Train")
+    );
+
+  // Append the new row to the table
+  $("#info").append(newRow);
 
   // Handle the errors
 }, function(errorObject) {
   console.log("Errors handled: " + errorObject.code);
 });
 
-database.ref().orderByChild("dateAdded").limitToLast(1).on("child-added", function(snapshot) {
-//       //change HTML
-$("#info").append('<tr><td>' + sv.name + '</td><td>' + sv.destination + '</td><td>' + sv.frequency + '</td><td>' + nextTrain + '</td><td>' + tMinutesTillTrain + '</td></tr>');
-}, function(errorObject) {
-  console.log("Errors handled: " + errorObject.code);
+$(document).on("click", ".remove-button", function(event){
+  database.ref(event.target.id).remove()
+  $(this).closest("tr").remove();
+
 })
-
-
-
-
-
